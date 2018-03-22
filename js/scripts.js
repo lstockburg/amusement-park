3ed91aee3d80f30e2ce5eea3e2bd162dada950ea
Brea Borlas and Luke Stockburger <luke@>
2018-03-22 10:26:34 -0700

    add else, else if

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
   var height = parseInt(prompt("Enter your height in inches:"));
-  if (height >= 60) {
-    $(".tall").css("background-color","#eaffa0")
+  if (height >= 77) {
+    $(".too-tall").css("background-color","#ff2626");
+  }
+  else if (height >= 60) {
+    $(".tall").css("background-color","#eaffa0");
+  }
+  else {
+    $(".short").css("background-color","#eaffa0");
   }
 });
